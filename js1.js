1bef7f943ec3e1cb3498e006fe43ebaaa70ce2c9
JomsReynaDaned <jomsreyna.daned@smc.pshs.edu.ph>
2025-01-29 23:51:17 +0800

    Add files via upload

--- js1.js
+++ js1.js
@@ -2,8 +2,8 @@ var name = prompt("Enter your nickname:");
 var height = parseInt(prompt("Enter your height in inches:"));
 var weight = parseFloat(prompt("Enter your weight in kg:"));
 
-var wholeFeet = parseInt(height / 12);
+var Feet = parseInt(height / 12);
 var inches = height % 12;
-var weightInLbs = weight * 2.20462;
+var weight = weight * 2.20462;
 
-alert("Name: " + name + "\n" +  "Height: " + wholeFeet + "'" + inches + "\"\n" +  "Weight: " + weightInLbs + " lbs");
+alert("Name: " + name + "\n" +  "Height: " + Feet + "'" + inches + "\"\n" +  "Weight: " + weight + " lbs");
--- js2.js
+++ js2.js
@@ -1,9 +1,9 @@
 var firstName = prompt("Enter your first name:");
 var lastName = prompt("Enter your last name:");
-var birthYear = parseInt(prompt("Enter your birth year:"));
+var birthyear = parseInt(prompt("Enter your birth year:"));
 
-var currentYear = 2025;
-var age = currentYear - birthYear;
+var currentyear = 2025;
+var age = currentyear - birthyear;
 
 document.getElementById("output").innerHTML = 
     "Hello " + firstName + " " + lastName + "! How does it feel to be " + age + " years old?";
